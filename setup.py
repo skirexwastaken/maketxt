ceb7b82798e090dfa1b82b990cc1be2ffa9d9d6d
Daniel <hora.daniel@protonmail.com>
2026-01-19 13:14:10 +0100

    Rename package and module from makeTXT to maketxt

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="makeTXT",
+    name="maketxt",
     version="1.0.0",
     # Use py_modules for single-file scripts
-    py_modules=["makeTXT"],
+    py_modules=["maketxt"],
     include_package_data=True,
     entry_points={
         "console_scripts": [
             # format: 'command=module:function'
-            "makeTXT=makeTXT:main",
+            "maketxt=maketxt:main",
         ],
     },
     python_requires=">=3.10",
