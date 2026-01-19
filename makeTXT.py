e4bdcc228ad4a2e50f44097bd632e88d0085261f
Daniel <hora.daniel@protonmail.com>
2026-01-19 13:09:53 +0100

    Update makeTXT.py

--- makeTXT.py
+++ makeTXT.py
@@ -1,20 +1,21 @@
 from pathlib import Path
 import sys
 
-if len(sys.argv) > 1:
-    with open(f"{Path.cwd()}/{sys.argv[1]}.txt","w") as file:
-        if len(sys.argv) == 2:
-            file.write("")
-
-            print(f"File '{sys.argv[1]}.txt' has been created.")
-
-        else:
-            textLine = " ".join(sys.argv[2:])
-            textLines = textLine.split("/")
-
-            for textLine in textLines:
-                file.write(f"{textLine}\n")
-
-            print(f"File '{sys.argv[1]}.txt' has been created with content {textLines}.")
-else:
-    print("Invalid filename.")
+def main():
+    if len(sys.argv) > 1:
+        with open(f"{Path.cwd()}/{sys.argv[1]}.txt","w") as file:
+            if len(sys.argv) == 2:
+                file.write("")
+    
+                print(f"File '{sys.argv[1]}.txt' has been created.")
+    
+            else:
+                textLine = " ".join(sys.argv[2:])
+                textLines = textLine.split("/")
+    
+                for textLine in textLines:
+                    file.write(f"{textLine}\n")
+    
+                print(f"File '{sys.argv[1]}.txt' has been created with content {textLines}.")
+    else:
+        print("Invalid filename.")
